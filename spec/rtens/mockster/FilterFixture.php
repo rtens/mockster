2871954e09d9b4f20c48d65313c631d3bd6bdbaa
Janosch Woschitz <janosch.woschitz@gmail.com>
2014-04-10 19:05:36 +0200

    added missing changes for filter test

--- spec/rtens/mockster/FilterFixture.php
+++ spec/rtens/mockster/FilterFixture.php
@@ -1,9 +1,51 @@
 <?php
 namespace spec\rtens\mockster;
 
-/**
- * @copyright ResearchGate GmbH
- */
-class FilterFixture {
+use rtens\mockster\filter\Filter;
+use watoki\scrut\Fixture;
 
-} 
+class FilterFixture extends Fixture {
+
+    /**
+     * @var Filter
+     */
+    private $filter;
+
+    /**
+     * @var array|\ReflectionMethod[]
+     */
+    private $filterOutput = [];
+
+    public function givenTheFilterWithTheBitMask($bitmask) {
+        $this->filter = new Filter($bitmask);
+    }
+
+    public function whenTheFilterIsAppliedToTheMethodsOfClass($class) {
+        $reflectionClass = new \ReflectionClass($class);
+        $filter = $this->filter;
+        $this->filterOutput = array_filter($reflectionClass->getMethods(), function($method) use ($filter) {
+            return $filter->apply($method);
+        });
+    }
+
+    public function thenTheFilterMatched_Methods($count) {
+        $this->spec->assertEquals($count, count($this->filterOutput));
+    }
+
+    public function thenFilterMatchesContainTheMethod($methodName) {
+        $this->spec->assertNotNull($this->getFilteredMethod($methodName));
+    }
+
+    /**
+     * @param string $methodName
+     * @return \ReflectionMethod|null
+     */
+    private function getFilteredMethod($methodName) {
+        foreach ($this->filterOutput as $method) {
+            if ($method->getName() == $methodName) {
+                return $method;
+            }
+        }
+        return null;
+    }
+}
