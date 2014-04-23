9d59ff769385b8a9062fa6491fa57664b88cfe33
Janosch Woschitz <janosch.woschitz@gmail.com>
2014-04-24 00:33:15 +0200

    modified MethodTypeHint to transcode short array notations to Traversable

--- spec/rtens/mockster/CheckReturnValueTest.php
+++ spec/rtens/mockster/CheckReturnValueTest.php
@@ -194,4 +194,26 @@ class CheckReturnValueTest extends Specification {
         $this->fixture->thenNoExceptionShouldBeThrown();
     }
 
-} 
+    public function testHintedAsArrayButReturnsTraversable() {
+        $this->fixture->givenTheClassDefinition('
+            class HintedAsArrayButReturnsTraversable {
+                /**
+                 * @return DateTime[]
+                 */
+                public function myFunction() {}
+            }
+        ');
+        $this->fixture->whenICreateTheMockOf('HintedAsArrayButReturnsTraversable');
+        $this->fixture->whenIConfigureTheMethod_ToReturn('myFunction', new TraversableCollection());
+        $this->fixture->whenITryToInvoke('myFunction');
+
+        $this->fixture->thenNoExceptionShouldBeThrown();
+    }
+}
+
+class TraversableCollection implements \IteratorAggregate {
+
+    public function getIterator() {
+        return new \ArrayIterator(array());
+    }
+}
